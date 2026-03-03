85dc9a7f47d18752d38d2ebf558f25d5d04599c2
AdonisChaban <chabanadonis@gmail.com>
2026-03-02 21:12:04 -0600

    finished with the python script

--- test/b3dFileBuilder.py
+++ test/b3dFileBuilder.py
@@ -5,46 +5,11 @@ from pathlib import Path
 class B3DBuilder:
     def __init__(self):
         self.data = bytearray()
-        self.chunk_stack = []
+        self.chunk_sizes = []
+        self.chunkp = []
+        self.bytesin = 0
 
 
-    # let it return the size of chunk
-    def parseNode(self):
-
-
-    def parseMesh(self):
-
-
-    def parseVRTS(self):
-        
-
-    def parseTRIS(self):
-        
-    
-    
-
-    def parseTEXS(self):
-        print(self.chunk_stack)
-        self.data += self.chunk_stack.pop(0)[1:5]
-        for i in range(len(self.chunk_stack) / 8):
-            self.chunk_size += len(self.chunk_stack[i * 8]) + 1
-            self.chunk_size += 28 # 28 bytes for storing the data
-            
-        self.size += chunk_size
-        self.data += struct.pack('<i', self.chunk_size)
-        self.chunk_size = 0
-
-        for i in range(len(chunk_stack) / 8):
-            self.data += chunk_stack.pop(0)
-            self.data += struct.pack('<i', int(chunk_stack.pop(0)))
-            self.data += struct.pack('<i', int(chunk_stack.pop(0)))
-            self.data += struct.pack('<f', float(chunk_stack.pop(0)))
-            self.data += struct.pack('<f', float(chunk_stack.pop(0)))
-            self.data += struct.pack('<f', float(chunk_stack.pop(0)))
-            self.data += struct.pack('<f', float(chunk_stack.pop(0)))
-            self.data += struct.pack('<f', float(chunk_stack.pop(0)))
-
-        print(self.chunk_stack) # should be done with parsing
         
                   
     def parseFile(self, filename):
@@ -56,52 +21,131 @@ class B3DBuilder:
         print(cl)
 
         #load bb3d shit into byte array
-        self.data += cl.pop(0)[1:5]
-        self.data += strcut.pack('<i', 1) # size of b3d file
-        self.data += struct.pack('<i', int(cl.pop(0)))
+        self.data += b'B3DD'
+        self.data += struct.pack('<i', 1) # size of b3d file
+        self.data += struct.pack('<i', int(cl[1]))
 
-        # we need to store the location of
+        # we need to store the location of the size of the b3d file,
+        # which is 5 bytes in
+        self.chunkp.append(4)
+        self.bytein += 12
 
-        # now we do some pattern matching
-        stop = false
-        
-        while stop:
-            match cl[0]:
-                case '[TEXS]':
-                    while cl[0] != 'END':
-                        self.chunk_stack.append(cl.pop(0))
-                    
-
-                    self.chunk_stack.append(cl.pop(0))
-                    self.parseTEXS()
+        for elem in cl:
+            try:
+                int(elem)
+            except ValueError:
+                try:
+                    float(elem[:-1])
+                except ValueError:
+                    # Must be a String!
+                    match elem:
+                        case '[TEXS]':
+                            self.data += b'TEXS'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
                             
-                        
-                case '[BRUS]':
 
-                    
-                case '[NODE]':
-                    if cl[0] == '[MESH]':
-                    
-                        
-                        
+                        case '[BRUS]':
+                            self.data += b'BRUS'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
+                            
 
-                    else if cl[0] == '[BONE]':
+                        case '[VRTS]':
+                            self.data += b'VRTS'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
+                            
 
+                        case '[TRIS]':
+                            self.data += b'TRIS'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
+                            
 
-                    else:
-                        print("Node type unknown")
-                        return
+                        case '[BONE]':
+                            self.data += b'BONE'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
+                            
+
+                        case '[KEYS]':
+                            self.data += b'KEYS'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
+                            
+
+                        case '[ANIM]':
+                            self.data += b'ANIM'
+                            self.bytein += 4
+                            self.chunkp.append(self.bytein)
+                            self.chunk_sizes.append(0)
+                            # store dummy size
+                            self.data += struct.pack('<i', 1)
+                            self.bytein += 4
+                            
+
+                        case 'END':
+                            # have a size of chunk, could be sub or not
+                            size = self.chunk_sizes.pop()
+                            # add the size to the parent chunks
+                            self.chunk_sizes = [x + size for x in self.chunk_sizes]
+                            location = self.chunkp.pop()
+                            self.data[location:location + 4] = struct.pack('<i',size)
+                            
+
+                        case string_name:
+                            # name for some chunk
+                            string_length = len(string_name) + 1
+                            self.byte_sizes[-1] += string_length
+                            self.data += string_name.encode("ascii")
+                            self.bytein += string_length
                         
-                        
-                        
-                        
-                    
-                case 'END':
-                    stop = True
-                
+                else:
+                    # must be a float
+                    self.data += struct.pack('<f', float(elem[:-1]))
+                    self.chunk_sizes[-1] += 4
+                    self.bytein += 4
+
+            else:
+                # must be an int
+                self.data += struct.pack('<i', int(elem))
+                self.chunk_sizes[-1] += 4
+                self.bytein += 4
+
+            # fill in total chunk size
+            size = chunk_sizes.pop()
+            self.data[4:8] = struct.pack('<i', size)
+            
                 
     
     def BuildB3DFile(self, filename):
+        with open(filename, "wb") as fb3d:
+            fb3d.write(self.data)
     
 
 
@@ -154,6 +198,7 @@ for b3d_text_file in file_list:
     if !Path(b3dfile).exists():
         # time to create a b3dfile
         builder = B3Dbuilder()
-        builder.parsefile()
+        builder.parsefile(b3d_text_file)
+        builder.BuildB3DFile(b3dfile)
     
     
